f5bc5eb7dc21e5dc7ab2a3357e0b491417c6ddea
Aaron Hurt <ahurt@anbcs.com>
2014-05-04 21:01:28 -0500

    add license/copyrights/doxygen blocks to top of each file // change indentation to tabs to match upstream style

--- couchbase.h
+++ couchbase.h
@@ -1,4 +1,27 @@
-/* couchbase */
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/*
+ * $Id$
+ *
+ * @brief Couchbase wrapper function prototypes and datatypes.
+ * @file couchbase.h
+ *
+ * @copyright 2013-2014 Aaron Hurt <ahurt@anbcs.com>
+ */
 
 #ifndef _couchbase_h_
 #define _couchbase_h_
@@ -10,15 +33,15 @@ RCSIDH(couchbase_h, "$Id$");
 
 /* struct to hold cookie data for couchbase callbacks */
 typedef struct cookie_t {
-    json_object *jobj;              /* json object */
-    json_tokener *jtok;             /* json tokener */
-    enum json_tokener_error jerr;   /* tokener error */
+	json_object *jobj;              /* json object */
+	json_tokener *jtok;             /* json tokener */
+	enum json_tokener_error jerr;   /* tokener error */
 } cookie_t;
 
 /* union of const and non const pointers */
 typedef union cookie_u {
-    const void *cdata;
-    void *data;
+	const void *cdata;
+	void *data;
 } cookie_u;
 
 /* general error callback */
@@ -26,11 +49,11 @@ void couchbase_error_callback(lcb_t instance, lcb_error_t error, const char *err
 
 /* store a key/document in couchbase */
 void couchbase_store_callback(lcb_t instance, const void *cookie, lcb_storage_t operation,
-    lcb_error_t error, const lcb_store_resp_t *item);
+	lcb_error_t error, const lcb_store_resp_t *item);
 
 /* get a document by key from couchbase */
 void couchbase_get_callback(lcb_t instance, const void *cookie, lcb_error_t error,
-    const lcb_get_resp_t *item);
+	const lcb_get_resp_t *item);
 
 /* create a couchbase instance and connect to the cluster */
 lcb_t couchbase_init_connection(const char *host, const char *bucket, const char *pass);
--- mod.h
+++ mod.h
@@ -1,4 +1,27 @@
-/* module utillity header */
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/*
+ * $Id$
+ *
+ * @brief Function prototypes and datatypes used in the module.
+ * @file mod.h
+ *
+ * @copyright 2013-2014 Aaron Hurt <ahurt@anbcs.com>
+ */
 
 #ifndef _mod_h_
 #define _mod_h_
@@ -18,21 +41,21 @@ RCSIDH(mod_h, "$Id$");
 
 /* configuration struct */
 typedef struct rlm_couchbase_t {
-    const char *acctkey;            /* accounting document key */
-    const char *doctype;            /* value of 'docType' element name */
-    char *server;                   /* couchbase server list */
-    const char *bucket;             /* couchbase bucket */
-    const char *pass;               /* couchbase bucket password */
-    unsigned int expire;            /* document expire time in seconds */
-    const char *userkey;            /* user document key */
-    json_object *map;               /* json object to hold user defined attribute map */
-    fr_connection_pool_t *pool;     /* connection pool */
+	const char *acctkey;            /* accounting document key */
+	const char *doctype;            /* value of 'docType' element name */
+	char *server;                   /* couchbase server list */
+	const char *bucket;             /* couchbase bucket */
+	const char *pass;               /* couchbase bucket password */
+	unsigned int expire;            /* document expire time in seconds */
+	const char *userkey;            /* user document key */
+	json_object *map;               /* json object to hold user defined attribute map */
+	fr_connection_pool_t *pool;     /* connection pool */
 } rlm_couchbase_t;
 
 /* connection pool handle struct */
 typedef struct rlm_couchbase_handle_t {
-    void *handle;    /* real couchbsae instance */
-    void *cookie;    /* couchbase cookie */
+	void *handle;    /* real couchbsae instance */
+	void *cookie;    /* couchbase cookie */
 } rlm_couchbase_handle_t;
 
 /* define functions */
